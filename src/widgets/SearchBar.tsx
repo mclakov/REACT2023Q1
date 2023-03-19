71de7e3bb364d8164c0357721478c0d1a66388c2
Roman Maklakov <mclakov@gmail.com>
2023-03-19 22:45:15 +0300

    feat: add seachbar component

---
+++ src/widgets/SearchBar.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+type SearchBarProps = Record<string, never>;
+
+type SearchBarState = Record<string, string>;
+
+class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({ searchValue: event.target.value });
+  }
+
+  componentDidMount() {
+    const localStorageValue = localStorage.getItem('searchValue');
+    if (localStorageValue) {
+      this.setState({ searchValue: localStorageValue });
+    }
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('searchValue', this.state.searchValue);
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          value={this.state.searchValue}
+          onChange={this.handleChange}
+          placeholder='Search'
+        ></input>
+        <button type='submit'>
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
