9436662435e06d29fe05414d51a2d2cfdb85984f
Roman Maklakov <mclakov@gmail.com>
2023-03-19 19:36:38 +0300

    feat: add pages

---
+++ src/pages/NFPage.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class
+
+404;
+Page;
+extends
+Component;
+{
+  render();
+  {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default 404;
+Page;
