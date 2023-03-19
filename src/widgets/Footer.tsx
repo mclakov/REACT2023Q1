59c87ca3495a935bd02e5389e8b254e81e08d9f8
Roman Maklakov <mclakov@gmail.com>
2023-03-19 22:10:11 +0300

    feat: add footer component

---
+++ src/widgets/Footer.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <a
+            href='https://rs.school/js/'
+          ></a>
+          <span>2023</span>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
