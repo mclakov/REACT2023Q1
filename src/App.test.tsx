aa452a19a7ff675f423c6c4ac8f2454287e6a890
Roman Maklakov <mclakov@gmail.com>
2023-03-20 23:40:39 +0300

    fix: tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import { MemoryRouter } from 'react-router-dom';
 
