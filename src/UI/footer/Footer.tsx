30eee19cbb139451265edda08a744f0c135d80cb
Roman Maklakov <mclakov@gmail.com>
2023-04-01 20:51:06 +0300

    fix: footer

--- src/UI/footer/Footer.tsx
+++ src/UI/footer/Footer.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from './Footer.module.scss';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className={style.footer}>
-        <div className={style.footerContainer}>
-          <a href="UI/footer/Footer" className={style.rsschool}></a>
-          <span>2023</span>
-        </div>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <div className={style.footerContainer}>
+        <a href="UI/footer/Footer" className={style.rsschool}></a>
+        <span>2023</span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
