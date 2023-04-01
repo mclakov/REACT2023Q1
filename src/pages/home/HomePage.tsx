37b489c84cb20e24ab512e97749d03af236fec3f
Roman Maklakov <mclakov@gmail.com>
2023-04-01 20:20:52 +0300

    fix: home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchBar from '../../UI/searchbar/SearchBar';
 import Cards from '../../UI/cards/Cards';
 import data from '../../assets/data/cardData.json';
 
-class HomePage extends Component {
-  render() {
-    return (
-      <div data-testid="home-page">
-        <SearchBar />
-        <Cards cards={data.data} />
-      </div>
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <div data-testid="home-page">
+      <SearchBar />
+      <Cards cards={data.data} />
+    </div>
+  );
+};
 
 export default HomePage;
