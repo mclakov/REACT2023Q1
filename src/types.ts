3cb8ba98fdda24a764afb1461c209af9f301ec28
Roman Maklakov <mclakov@gmail.com>
2023-04-01 20:45:09 +0300

    fix: card

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,15 @@ export type TUserCardModel = {
 };
 
 export type TFormPageProps = Record<string, never>;
+
+export type TCardProps = {
+  id: string;
+  title: string;
+  author: string;
+  url: string;
+  date: string;
+  views: string;
+  likes: string;
+  dislikes: string;
+  comments: string;
+};
