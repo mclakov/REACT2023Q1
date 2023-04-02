12203788ebf968b7b47e4382b166b377484c1cf8
Roman Maklakov <mclakov@gmail.com>
2023-04-02 20:47:46 +0300

    fix: layout

--- src/UI/layout/Layout.tsx
+++ src/UI/layout/Layout.tsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import style from './Layout.module.scss';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
-class Layout extends Component {
-  render() {
-    return (
-      <div className={style.wrapper}>
-        <Header />
-        <main className={style.main}>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    );
-  }
-}
+const Layout = () => {
+  return (
+    <div className={style.wrapper}>
+      <Header />
+      <main className={style.main}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
