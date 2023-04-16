873d3f66c28286373fec58e8078c02331b4daa7f
Roman Maklakov <mclakov@gmail.com>
2023-04-17 00:58:48 +0300

    feat: add form slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from '../features/search';
 import detailReducer from '../features/detail';
+import formReducer from '../features/form';
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
     detail: detailReducer,
+    form: formReducer,
   },
 });
 
