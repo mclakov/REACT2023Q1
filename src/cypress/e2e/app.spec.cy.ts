4a290200aeef6f22e872ea1c762b65bbea7896e1
Roman Maklakov <mclakov@gmail.com>
2023-05-01 00:09:22 +0300

    feat: add cypress

---
+++ src/cypress/e2e/app.spec.cy.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+describe('Test app functionality', () => {
+  it('navigation works', () => {
+    cy.visit('/');
+
+    cy.get('a[href="/about"]').click();
+    cy.url().should('include', '/about');
+
+    cy.get('a[href="/forms"]').click();
+    cy.url().should('include', '/forms');
+
+    cy.visit('/ttt');
+    cy.get('h1').should('contain', '404');
+  });
+});
